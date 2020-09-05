ab5a7ae2db6260fa584193bb908fb0027d68f05e
Ian Macaulay <ismacaul@gmail.com>
2020-09-04 19:39:27 -0600

    Implemented basic item db operations

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -187,21 +187,114 @@ func (db *PostgresDatabase) DeleteList(user, list_id string) error {
 }
 
 func (db *PostgresDatabase) RetrieveAllItems(user, list_id string) ([]models.Item, error) {
-	return []models.Item{}, nil
+	sqlStatement := `
+		SELECT i.id, i.title, i.description
+		FROM items i
+		INNER JOIN lists l ON (i.list_id = l.id)
+		WHERE l.user_id = $1 AND i.list_id = $2
+		ORDER BY i.date_created DESC`
+
+	rows, err := db.conn.Query(sqlStatement, user, list_id)
+	if err != nil {
+		log.Printf("Failed to load items for user %s\nError: %s\n", user, err.Error())
+		return []models.Item{}, ErrFailedToLoadData
+	}
+	defer rows.Close()
+
+	items := make([]models.Item, 0)
+	for rows.Next() {
+		var id uuid.UUID
+		var title, description string
+		if err := rows.Scan(&id, &title, &description); err != nil {
+			log.Printf("Failed to scan row: %s\n", err.Error())
+			return []models.Item{}, ErrFailedToScanRow
+		}
+
+		item := models.Item{
+			Id:          &id,
+			Title:       &title,
+			Description: &description,
+		}
+		items = append(items, item)
+	}
+
+	return items, nil
 }
 
 func (db *PostgresDatabase) RetrieveItem(user, list_id, item_id string) (models.Item, error) {
-	return models.Item{}, nil
+	sqlStatement := `
+		SELECT i.id, i.title, i.description
+		FROM items i
+		INNER JOIN lists l ON (i.list_id = l.id)
+		WHERE l.user_id = $1 AND i.list_id = $2 AND i.id = $3
+		ORDER BY i.date_created DESC`
+
+	var listId uuid.UUID
+	var title, description string
+	err := db.conn.QueryRow(sqlStatement, user, list_id, item_id).Scan(&listId, &title, &description)
+	if err != nil {
+		log.Printf("Failed to execute query: %s\n", err.Error())
+		return models.Item{}, ErrFailedToLoadData
+	}
+
+	item := models.Item{
+		Id:          &listId,
+		Title:       &title,
+		Description: &description,
+	}
+	return item, nil
 }
 
 func (db *PostgresDatabase) CreateItem(user, list_id string, item models.Item) error {
+	now := time.Now().UTC().Unix()
+
+	sqlStatement := `
+		INSERT INTO items (id, date_created, date_modified, title, description, list_id)
+		VALUES ($1, $2, $3, $4, $5, $6)`
+
+	_, err := db.conn.Exec(sqlStatement, item.Id, now, now, item.Title, item.Description, list_id)
+	if err != nil {
+		log.Println("Failed to create item:", err)
+		return ErrFailedToInsert
+	}
+
 	return nil
 }
 
 func (db *PostgresDatabase) UpdateItem(user, list_id string, item models.Item) error {
+	now := time.Now().UTC().Unix()
+
+	sqlStatement := `
+		UPDATE items
+		SET date_modified = $4, title = $5, description = $6
+		FROM lists
+		WHERE lists.user_id = $1
+			AND items.list_id = $2
+			AND items.id = $3`
+
+	_, err := db.conn.Exec(sqlStatement,
+		user, list_id, item.Id, now, item.Title, item.Description,
+	)
+	if err != nil {
+		log.Println("Failed to update item:", err)
+		return ErrFailedToUpdateData
+	}
+
 	return nil
 }
 
 func (db *PostgresDatabase) DeleteItem(user, list_id, item_id string) error {
+	sqlStatement := `
+		DELETE FROM items
+		USING lists
+		WHERE lists.user_id = $1
+			AND items.list_id = $2
+			AND items.id = $3`
+
+	_, err := db.conn.Exec(sqlStatement, user, list_id, item_id)
+	if err != nil {
+		log.Println("Failed to update item:", err)
+		return ErrFailedToUpdateData
+	}
 	return nil
 }
